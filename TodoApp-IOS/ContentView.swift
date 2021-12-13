4fa37bcc2f748eb3a4e393ae9c0097786d6a4c7b
Samuel Noye <samuel.noye@amalitech.org>
2021-12-13 09:18:34 +0000

    Initial Commit

---
+++ TodoApp-IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoApp-IOS
+//
+//  Created by Samuel Noye on 13/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoApp-IOS/TodoApp_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_IOSApp.swift
+//  TodoApp-IOS
+//
+//  Created by Samuel Noye on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
